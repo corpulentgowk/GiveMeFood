69ffb4b14da2a1249fc5b035d16bb7d70e20461a
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-07-14 14:11:15 -0700

    added cookies and sessions

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 // configuration ===========================================
 	
@@ -19,6 +20,29 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+app.use(session({
+  secret: 'weDefinitelyDoesThis',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false, expires:false, httpOnly: false }
+}))
+
+/*
+Debug Line for checking if cookie remains static
+app.all('/tez',function(req,res,next){
+    var session = req.session;
+    if(session.count){
+        session.count++;
+    }
+    else{
+        session.count = 1;
+    }
+    console.log('id:',req.sessionID);
+    console.log('count:',session.count);
+    res.end();
+});*/
+
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
