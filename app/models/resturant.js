c0a6ce7ebfb6010a7a19daeeee74a58c4a831e82
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-07-18 09:50:56 -0700

    Added schema requirments for menus to Resturant model

--- app/models/resturant.js
+++ app/models/resturant.js
@@ -4,10 +4,22 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var resturantSchema = new Schema({
-  
-    name: {type:String, required: true, index:true},
+
+    name: {type: String, index: true, unique: true, required: true},
     image_url: {type:String, required: true},
-    menus: {type: [{}]}
+    menus:[
+		  {
+		  	name: {
+		    type: String, 
+		    required: true, //name for menu is required
+		    unique: true,
+		    index: true
+		  },
+		  items: { 
+		    type: Array,
+		    required: true
+		  }
+		}]
 });
 
 var resturant = mongoose.model('Resturant', resturantSchema);
