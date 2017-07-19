1a2e4d6605ebae9d6a52d3f39b568afd62e82f2e
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-07-19 10:14:21 -0700

    enforced uniqueness of email addreses

--- app/models/user.js
+++ app/models/user.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
   
     name: {type:String, required: true, index:true},
-    email: {type:String, required: true},
+    email: {type:String, required: true, unique:true},
     password: {type: String, required:true},
     admin: {type: Boolean, default:false}
 });
