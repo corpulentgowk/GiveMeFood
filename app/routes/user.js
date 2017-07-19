364b13d52ebbc695afe9486ad7c94c7cbea2fb9b
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-07-19 10:15:13 -0700

    Added user password encryption

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,32 +2,52 @@ var express = require('express');
 var router = express.Router();
 
 var User = require('../models/user');
+var encrypt = require('../../config/encryptTool')
 
 router.post('/signup', function(req,res){
   console.log(req.body);
-  User.create(req.body, function (err, item) {
-     if (err){
-      res.json(err);
-      return;
-     } 
-        // saved!
-        res.json({"status":"success"});
+
+  encrypt.cryptPassword(req.body.password, function(err, hash){
+      if (!err){
+        req.body.password = hash;
+        console.log(req.body.password);
+        User.create(req.body, function (err, item) {
+           if (err){
+            res.json(err);
+            return;
+           } 
+              res.json({"status":"success"});
+          });
+      }
+      else{
+        res.json({"status":"There was an unexpected error creating your account. Please try again."})
+      }
     });
 });
 
 router.post('/login', function (req, res, next) {
-   var email = req.body.email;
+   var emailAddr = req.body.email;
    var pass = req.body.pass;
 
-   User.findOne(req.body, function(err, user) {
+   User.findOne({email:emailAddr}, function(err, user) {
       if(err) return next(err);
       if(!user) return res.send('Not logged in!');
-
-      req.session.user = user;
-
-      return res.send('Logged In!');
-   });
+      encrypt.comparePassword(req.body.password, user.password, 
+        function(err, isMatch){
+            if (err){res.json(err); return;}
+            if (isMatch){
+                req.session.user = user;
+                return res.json({"status":"Logged In!"});
+            }
+            else{
+              req.session.user = null;
+              return res.json({"status":"Password Incorrect"})
+            }
+          }
+        );
+    });
 });
+
 
 router.get('/logout', function (req, res) {
    req.session.user = null;
@@ -36,7 +56,7 @@ router.get('/logout', function (req, res) {
 
 router.get('/current_user', function (req, res) {
    //req.session.user = null;
-   return res.send(req.session.user)
+   return res.send(req.session.user.email)
 });
 
 router.get('/is_admin', function (req, res) {
